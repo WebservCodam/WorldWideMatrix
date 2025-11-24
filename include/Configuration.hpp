3f353e2bb9bee82dd5a65051203d0d1dcd6a77a8
Lisandro <lisandroprieri@gmail.com>
2025-11-24 18:14:24 +0100

    changes in webserv

--- include/Configuration.hpp
+++ include/Configuration.hpp
@@ -51,8 +51,6 @@ class	Directive
 		void setContext(const std::string& context);
 		void setParameters(const std::vector<std::string>& parameters);
 		void addChild(std::unique_ptr<Directive> child);
-
-		
 };
 
 class	ConfigFile
@@ -71,6 +69,44 @@ class	ConfigFile
 		const Directive*				findDirective(const std::string& name) const;
 		std::vector<const Directive*>	findAllDirectives(const std::string& name) const;
 };
+
+class	Location
+{
+	private:
+		std::string					_path;
+		std::string					_root;
+		std::string					_index;
+		bool						_autoindex;
+		// std::vector<std::string>	_allowedMethods;
+		bool						_getMethod;
+		bool						_postMethod;
+		bool						_deleteMethod;
+
+	public:
+		Location() = delete;
+		Location(const std::string& path, const std::string& root = "", const std::string& index = "", bool autoindex = false, bool getMethod = true, bool postMethod = false, bool deleteMethod = false);
+		~Location() = default;
+
+
+};
+
+class	Server
+{
+	private:
+		std::string								_serverName;
+		std::map<std::string, std::string>		_addressesAndPorts;	//Defaults to 0.0.0.0:80
+		size_t									_maxBodySize;
+		std::map<std::vector<int>, std::string>	_error;	//	The idea is that different error codes can return the same error. But this might overcomplicate things.
+		std::vector<Location>					_location;
+
+	public:
+		Server() = default;
+		Server(const std::string& serverName, const std::map<std::string, std::string>& addressesAndPorts = {{"0.0.0.0", "80"}}, size_t maxBodySize = 1048576, const std::map<std::vector<int>, std::string>& error = {}, const std::vector<Location>& location = {});
+		~Server() = default;
+
+};
+
+
 
 enum TokenType
 {
