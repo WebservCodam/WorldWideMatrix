300dbc2287e606430b88351655d348f57ad6804f
Lisandro <lisandroprieri@gmail.com>
2026-02-10 14:37:52 +0100

    Remove limit except dead code. Started migrating validation functions to their own files.

---
+++ configparser/src/config/validation functions/ListenDirective.cpp
@@ -0,0 +1,33 @@
+#include "../../../include/Configuration.hpp"
+
+bool	validateListenDirective(const Directive* node)
+{
+
+	std::cout << "DEBUG: In validateListenDirective" << std::endl;
+
+	std::pair<std::string, std::string>	addressAndPort;	//127.0.0.0:8080?
+	bool								isValidAddress;
+	bool								isValidPort;
+
+	if (node->getParameters().empty())
+		throw ConfigError::validation("Directive '" + node->getName() + "' requires at least one parameter", node);
+
+	addressAndPort = parseAddressAndPort(node->getParameter(0));
+	if (addressAndPort.first.empty() && addressAndPort.second.empty())
+	{
+		if (validateAddress(node->getParameter(0)))
+			return (true);
+		if (validatePort(node->getParameter(0)))
+			return (true);
+		throw ConfigError::validation("Invalid address or port format in '" + node->getName() + "' directive: '" + node->getParameter(0) + "'", node);
+	}
+	isValidAddress = validateAddress(addressAndPort.first);
+	isValidPort = validatePort(addressAndPort.second);
+	if (isValidAddress && addressAndPort.second.empty())
+		return (true);
+	else if (isValidAddress && isValidPort)
+		return (true);
+	else if (addressAndPort.first.empty() && isValidPort)
+		return (true);
+	throw ConfigError::validation("Invalid address:port combination in '" + node->getName() + "' directive: '" + node->getParameter(0) + "'", node);
+}
---
+++ configparser/src/config/validation functions/Utilities.cpp
@@ -0,0 +1,78 @@
+#include "../../../include/Configuration.hpp"
+
+std::pair<std::string, std::string>	parseAddressAndPort(const std::string& address)
+{
+	std::pair<std::string, std::string>	addressAndPort = {"", ""};
+	std::string	addressPart;
+	std::string	portPart;
+	size_t		pos;
+
+	if (address.empty())
+		return (addressAndPort);
+	pos = address.find(":");
+	if (pos == address.npos)
+		return (addressAndPort);
+	else
+	{
+		addressPart = address.substr(0, pos);
+		portPart = address.substr(pos + 1, address.size() - (pos + 1));
+		addressAndPort = {std::move(addressPart), std::move(portPart)};
+	}
+
+	std::cout << "DEBUG in parserAddressAndPort - Address: " << addressAndPort.first << " Port: " << addressAndPort.second << std::endl;
+
+	return (addressAndPort);
+}
+
+bool	validateAddress(const std::string& address)
+{
+	size_t		currentPos = 0;
+	size_t		nextPos;
+	size_t		iterations = 0;
+	std::string	currentChunk;
+
+	if (address.empty())
+		return (false);
+
+	if (address == "localhost")
+		return (true);
+
+	while (currentPos < address.length())
+	{
+		nextPos = address.find(".", currentPos);
+		if (nextPos == std::string::npos)
+			currentChunk = address.substr(currentPos);
+		else
+			currentChunk = address.substr(currentPos, nextPos - currentPos);
+
+		if (!isByte(currentChunk))
+			return (false);
+
+		iterations++;
+		if (nextPos == std::string::npos)
+			break;
+		currentPos = nextPos + 1;
+	}
+	if (iterations != 4)
+		return (false);
+	return (true);
+}
+
+bool	validatePort(const std::string& port)
+{
+	if (port.empty())
+		return (false);
+	try
+	{
+		int portNumber = std::stoi(port);
+		if (port.size() != std::to_string(portNumber).length())
+			throw (std::runtime_error("Wrong port"));
+		if (portNumber >= 1 && portNumber <= 65535)
+			return (true);
+	}
+	catch(const std::exception& e)
+	{
+		return (false);
+	}
+	return (false);
+}
