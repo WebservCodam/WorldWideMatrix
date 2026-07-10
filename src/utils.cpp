fd014d0a813bc22522888752ba8af4578dbcc6ae
vknape <vtknape@gmail.com>
2026-07-10 11:03:27 +0200

    Fixed fd leak because of wrong fcntl implementation

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   utils.cpp                                          :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: vknape <vknape@student.codam.nl>             +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2025/09/15 13:53:26 by vknape        #+#    #+#                 */
-/*   Updated: 2026/06/18 13:31:34 by lprieri       ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   utils.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vknape <vknape@student.codam.nl>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 13:53:26 by vknape            #+#    #+#             */
+/*   Updated: 2026/07/10 11:02:06 by vknape           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 void	setNonBlocking(int fd)
 {
-	if (fcntl(fd, F_SETFL, O_NONBLOCK, FD_CLOEXEC) == -1)
+	if (fcntl(fd, F_SETFL, O_NONBLOCK) == -1)
 	{
 		throw std::runtime_error("fcntl error. Failure to set socket to non-blocking.");
+	}
+	if (fcntl(fd, F_SETFD, FD_CLOEXEC) == -1)
+	{
+		throw std::runtime_error("fcntl error. Failure to set socket to close on execute.");
 	}
 }
